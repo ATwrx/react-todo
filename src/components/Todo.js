c8bf45d25bac3d4037548b58e2a6c1b31704162d
atwrx <act.webdesigns@gmail.com>
2019-10-01 00:58:06 -0500

    improce syntax in Todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,16 +5,14 @@ export const Todo = props => {
   const { todoList } = props
   return (
     <ul>
-      {todoList.map(({ title, desc, created }, i) => {
-        return (
-          <li key={i}>
-            <p>
-              <b>{title}: </b>
-              {desc} (created on {created})
-            </p>
-          </li>
-        )
-      })}
+      {todoList.map(({ title, desc, created }, i) => (
+        <li key={i}>
+          <p>
+            <b>{title}: </b>
+            {desc} (created on {created})
+          </p>
+        </li>
+      ))}
     </ul>
   )
 }
